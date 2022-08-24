b59203ac5ae160cdb4c9511575c4e092d2beb866
Mr.Peerapong <peerapong.dev.369@gmail.com>
2022-08-25 02:18:09 +0700

    change database.py

--- models/database.py
+++ models/database.py
@@ -15,7 +15,7 @@ else:
     DATABASE_PASSWORD = config("DATABASE_PASSWORD")
     DATABASE_NAME = config("DATABASE_NAME")
     DATABASE_HOST = config("DATABASE_HOST")
-    SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://(DATABASE_USER):(DATABASE_PASSWORD)@(DATABASE_HOST)/(DATABASE_NAME)'
+    SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg2://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}'
     engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
